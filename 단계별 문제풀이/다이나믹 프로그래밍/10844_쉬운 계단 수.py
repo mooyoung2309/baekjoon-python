a0303ee01f802d8dcd94eb640c21066c44af99fc
mooyoung2309 <mooyoung2309@gmail.com>
2021-02-05 20:11:47 +0900

    21/02/05

--- 단계별 문제풀이/다이나믹 프로그래밍/10844_쉬운 계단 수.py
+++ 단계별 문제풀이/다이나믹 프로그래밍/10844_쉬운 계단 수.py
@@ -1,14 +1,20 @@
 import sys
-import collections
-from typing import List
+input = sys.stdin.readline
 
-input=sys.stdin.readline
+# make dp
+dp = []
+dp.append([1 for i in range(10)])
+for i in range(99):
+  dp.append([0 for i in range(10)])
 
-N = int(input())
+# iterate dp
+for i in range(99):
+  for j in range(10):
+    if j >= 1:
+      dp[i+1][j-1] += dp[i][j]
+    if j <= 8:
+      dp[i+1][j+1] += dp[i][j]
 
-dp = collections.defaultdict(list)
-dp
-print(dp)
-
-start = 1
-nums = []
+# input & output
+n = int(input())
+print((sum(dp[n-1])-dp[n-1][0])%1000000000)
